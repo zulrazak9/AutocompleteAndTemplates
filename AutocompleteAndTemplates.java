5977d1356627dc21245414376d51299c83449960
My ASUS <zulrazak9@gmail.com>
2023-10-29 23:33:14 +0700

    add

---
+++ AutocompleteAndTemplates.java
@@ -0,0 +1,32 @@
+import com.sun.tools.javac.Main;
+
+import java.util.Scanner ;
+public class AutocompleteAndTemplates {
+    public String nama ;
+    public int tanggallahir ;
+    public int bulanlahir ;
+    public int tahunlahir ;
+
+ public void input (){
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Masukan nama : ");
+         nama = scanner.nextLine();
+         System.out.print("Masukan Tanggal Lahir : ");
+         tanggallahir = scanner.nextInt();
+         System.out.print("Masukan Bulan Lahir : ");
+         bulanlahir = scanner.nextInt();
+         System.out.print("Masukan Tahun Lahir : ");
+         tahunlahir = scanner.nextInt();
+     }
+     public int HitungUmur (int tahun){
+             return  2023 - tahun ;
+             }
+
+
+    public static void main(String[] args) {
+        AutocompleteAndTemplates main = new AutocompleteAndTemplates();
+        main.input();
+        System.out.println(main.HitungUmur(main.tahunlahir));
+
+    }
+}
